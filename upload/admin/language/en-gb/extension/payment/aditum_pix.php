df54006fe34f79c2444018fffdd835ef3265579d
Everaldo <albreisdev@gmail.com>
2022-04-18 14:54:45 +0000

    Language pix admin

---
+++ upload/admin/language/en-gb/extension/payment/aditum_pix.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']      = 'Aditum - Pix';
+
+// Text
+$_['text_extension']     = 'Extensions';
+$_['text_success']       = 'Success: You have modified Aditum - Pix payment module!';
+$_['text_edit']          = 'Edit Aditum - Pix';
+
+// Entry
+$_['entry_total']        = 'Total';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Help
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment Aditum - Pix!';
+
+$_['aditum_pix_modo'] = 'Ambiente do Gateway';
+$_['aditum_pix_titulo_gateway'] = 'Título do Gateway';
+$_['aditum_pix_descricao_gateway'] = 'Descrição do Gateway';
+$_['aditum_pix_instrucoes'] = 'Instruções Após o Pedido';
+$_['aditum_pix_cnpj'] = 'CNPJ';
+$_['aditum_pix_merchant_token'] = 'Merchant Token';
+$_['aditum_pix_campo_documento'] = 'Campo Documento (CPF/CNPJ)';
+$_['aditum_pix_campo_numero'] = 'Campo Número da Rua';
+$_['aditum_pix_campo_complemento'] = 'Campo Complemento';
+$_['aditum_pix_tipo_antifraude'] = 'Tipo de Antifraude';
+$_['aditum_pix_token_antifraude'] = 'Token';
